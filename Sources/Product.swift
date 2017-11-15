7006eff58970ce9a84e3d17fd1444144d7b113ad
Elviro Rocca <elviro.rocca@facile.it>
2017-11-15 15:18:59 +0100

    FPI-8 added CoproductType + concrete Coproduct

--- Sources/Product.swift
+++ Sources/Product.swift
@@ -5,7 +5,31 @@ public protocol ProductType {
 	func fold<T>(_ transform: @escaping (FirstType,SecondType) -> T) -> T
 }
 
+public struct Product<A,B>: ProductType {
+	private let _first: A
+	private let _second: B
+
+	public init(_ first: A, _ second: B) {
+		self._first = first
+		self._second = second
+	}
+
+	public func fold<T>(_ transform: @escaping (A, B) -> T) -> T {
+		return transform(_first,_second)
+	}
+}
+
+extension Product where A: Equatable, B: Equatable {
+	public static func == (lhs: Product, rhs: Product) -> Bool {
+		return lhs.fold(identity) == rhs.fold(identity)
+	}
+}
+
 extension ProductType {
+	public var toProduct: Product<FirstType,SecondType> {
+		return fold(Product<FirstType,SecondType>.init)
+	}
+
 	public var first: FirstType {
 		return fold { first, _ in first }
 	}
@@ -28,25 +52,5 @@ extension ProductType {
 
 	public func mapSecond<U>(_ transform: @escaping (SecondType) -> U) -> Product<FirstType,U> {
 		return fold { first, second in Product<FirstType,U>.init(first, transform(second)) }
-	}
-}
-
-public struct Product<A,B>: ProductType {
-	private let _first: A
-	private let _second: B
-
-	public init(_ first: A, _ second: B) {
-		self._first = first
-		self._second = second
-	}
-
-	public func fold<T>(_ transform: @escaping (A, B) -> T) -> T {
-		return transform(_first,_second)
-	}
-}
-
-extension Product where A: Equatable, B: Equatable {
-	public static func == (left: Product, right: Product) -> Bool {
-		return left.fold(identity) == right.fold(identity)
 	}
 }
